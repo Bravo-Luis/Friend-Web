5c00ec89f01a2c0a7ad7358c072be407be351937
Bravo-Luis <luisbravo@ucsb.edu>
2022-10-15 16:34:05 -0700

    Initial Commit
    Created Project

---
+++ Friend Web/Friend Web/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Friend Web
+//
+//  Created by Luis Bravo on 10/15/22.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack{
+            
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Friend Web/Friend Web/Friend_WebApp.swift
@@ -0,0 +1,18 @@
+//
+//  Friend_WebApp.swift
+//  Friend Web
+//
+//  Created by Luis Bravo on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Friend_WebApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
